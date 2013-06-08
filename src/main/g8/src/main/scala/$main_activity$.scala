627b187ee7eaf583dddca5760075e9191d668721
taisukeoe <oeuia.t@gmail.com>
2013-06-08 22:12:12 +0900

    modified bug that TypedResources nature was not removed properly

--- src/main/g8/src/main/scala/$main_activity$.scala
+++ src/main/g8/src/main/scala/$main_activity$.scala
@@ -2,9 +2,11 @@ package $package$
 
 import _root_.android.app.Activity
 import _root_.android.os.Bundle
+import _root_.android.widget.TextView
 
-class $main_activity$ extends Activity with TypedActivity {
+class $main_activity$ extends Activity{
   override def onCreate(bundle: Bundle) {
+    if(BuildConfig.DEBUG) Log.d("$name$","$main_activity$ onCreate")
     super.onCreate(bundle)
     setContentView(R.layout.main)
 
